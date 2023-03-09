25632913c32417832dd6536c29352d21324ec992
Nice22 <benothouessou74@gmail.com>
2023-03-09 05:27:15 -0800

    a program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
+# Nice22
 
 if __name__ == "__main__":
-    find_var()
+    """a program that imports the variable a from a file"""
+    from variable_load_5 import a
 
+    print("{:d}".format(a))
