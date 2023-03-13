ad4e1ab745a089c2a6acc927e0e66edc5e899fdd
Nice22 <benothouessou74@gmail.com>
2023-03-13 11:32:38 +0000

    a function in C that checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/* Nice22 */
+
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	return (0);
+}
