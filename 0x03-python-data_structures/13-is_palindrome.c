2aa9d0a87873386ce1e5b23b8aebada979a6e05b
Nice22 <benothouessou74@gmail.com>
2023-03-13 12:50:05 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,14 +3,43 @@
 #include "lists.h"
 
 /* Nice22 */
-
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head;
+  * palindrome - check if is palindrome with recursion
+  * @l: l
+  * @r: r
+  *
+  * Return: 1 palindrome, 0 not palindrome
+  */
+ int palindrome(listint_t **l, listint_t *r)
+ {
+ 	int response;
 
-	return (0);
-}
+ 	if (r != NULL)
+ 	{
+ 		response = palindrome(l, r->next);
+ 		if (response != 0)
+ 		{
+ 			response = (r->n == (*l)->n);
+ 			*l = (*l)->next;
+ 			return (response);
+ 		}
+ 		return (0);
+
+ 	}
+ 	return (1);
+ }
+
+ /**
+  * is_palindrome - checks if a singly linked list is a palindrome.
+  * @head: head of linked list
+  *
+  * Return: 1 palindrome, 0 not palindrome
+  */
+ int is_palindrome(listint_t **head)
+ {
+ 	if (head == NULL)
+ 	{
+ 		return (0);
+ 	}
+ 	return (palindrome(head, *head));
+ }
