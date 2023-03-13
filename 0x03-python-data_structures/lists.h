22786b9c69fae7aa9b0448294055b0ad41245028
Nice22 <benothouessou74@gmail.com>
2023-03-13 13:00:24 -0700

    lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,4 +1,4 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
@@ -11,10 +11,9 @@ fndef LISTS_H
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
