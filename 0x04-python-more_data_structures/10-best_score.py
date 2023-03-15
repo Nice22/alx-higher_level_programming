8b64e8a27feb1ce63dfddf436f60af905bf1a625
Nice22 <benothouessou74@gmail.com>
2023-03-15 09:24:55 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(my_dict):
         return None
     biggest = max(my_dict.values())
     for key, value in my_dict.items():
-        if value ii biggest:
-            return key 
+        if value is biggest:
+            return key
